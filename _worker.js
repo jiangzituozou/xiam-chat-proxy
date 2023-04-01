fa735cf993497f9ae9ccf9441121ec9fe11fa072
我的菜鸟程序员 <44707091+jiangzituozou@users.noreply.github.com>
2023-04-01 16:06:01 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,9 +1,9 @@
 export default {
   async fetch(request, env) {
         console.log('test')
-    console.log(request.body)
+    console.log(request.url)
     const url = new URL(request.url);
-    url.host = "api.openai.com";
+    url.host = "https://api.openai.com/v1/chat/completions";
     // openai is already set all CORS heasders 
     return fetch(url, {
       headers: request.headers,
