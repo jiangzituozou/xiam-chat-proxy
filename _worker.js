c5c5c53b4880622be2da86cf1146d32ca4f707c7
我的菜鸟程序员 <44707091+jiangzituozou@users.noreply.github.com>
2023-04-01 15:46:02 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,6 @@
 export default {
   async fetch(request, env) {
-    console.log(request)
-    console.log(env)
+    console.log(request.body)
     const url = new URL(request.url);
     url.host = "api.openai.com";
     // openai is already set all CORS heasders 
